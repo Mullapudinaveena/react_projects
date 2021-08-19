62106bc1d5cb60e76fe4eadcf0d48c9d2ac23bd5
Naveena_m <16699169+Mullapudinaveena@users.noreply.github.com>
2021-08-18 20:46:01 -0500

    Add files via upload

---
+++ project_4/src/reducers/index.js
@@ -0,0 +1,30 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+    return [
+        {
+            title: 'Scrubs', duration: '4:05'
+        },
+        {
+            title: 'Macarena', duration: '2:30'
+        },
+        {
+            title: 'All Star', duration: '1:45'
+        },
+        {
+            title: 'backstreet', duration: '3:15'
+        }
+    ]
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
